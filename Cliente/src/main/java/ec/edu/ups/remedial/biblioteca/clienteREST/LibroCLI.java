e51b14065d27e0aca651b11fc319935d3ce22554
bryamOzl <bryan95_19@hotmail.com>
2021-02-27 15:41:52 -0500

    Consola

--- Cliente/src/main/java/ec/edu/ups/remedial/biblioteca/clienteREST/LibroCLI.java
+++ Cliente/src/main/java/ec/edu/ups/remedial/biblioteca/clienteREST/LibroCLI.java
@@ -1,26 +1,45 @@
 package ec.edu.ups.remedial.biblioteca.clienteREST;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 
 import ec.edu.ups.remedial.biblioteca.modelo.Libro;
 
 
 public class LibroCLI {
 	
-	private String WS_GET_VEHICULO = "http://localhost:8080/biblioteca/ws/libro/buscarL";
+	private String WS_GET_LIBRO = "http://localhost:8080/biblioteca/ws/libro/buscarL";
+	private String WS_GET_LIBROS = "http://localhost:8080/biblioteca/ws/libro/listarl";
+
 	
 	public Libro getLibro(int libro_id) {
 		Client client = ClientBuilder.newClient();
 
-		WebTarget target = client.target(WS_GET_VEHICULO).queryParam("libro_id", libro_id);
+		WebTarget target = client.target(WS_GET_LIBRO).queryParam("libro_id", libro_id);
 
 		Libro libro = target.request().get(Libro.class);
 
 		client.close();
 
 		return libro;
+	}
+	
+	public List<Libro> listarLibro(int categoria_id) {
+		List<Libro> listarLibro = new ArrayList<Libro>();
+		Client client = ClientBuilder.newClient();
+
+		WebTarget target = client.target(WS_GET_LIBROS).queryParam("categoria_id", categoria_id);
+
+		listarLibro = target.request().get(new GenericType<List<Libro>>() {
+		});
+
+		client.close();
+		return listarLibro;
 	}
 
 }
--- Cliente/src/main/java/ec/edu/ups/remedial/biblioteca/clienteREST/Reserva.java
+++ Cliente/src/main/java/ec/edu/ups/remedial/biblioteca/clienteREST/Reserva.java
@@ -12,8 +12,12 @@ import ec.edu.ups.remedial.biblioteca.modelo.Libro;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Reserva extends JFrame {
 
@@ -22,6 +26,9 @@ public class Reserva extends JFrame {
 	private JTextField nombreTXT;
 	private JTextField stockTXT;
 	private JTextField estadoTXT;
+	private JTable table;
+	private JTextField categoriaIDTXT;
+	private JButton categoriaBNT;
 
 	/**
 	 * Launch the application.
@@ -44,12 +51,12 @@ public class Reserva extends JFrame {
 	 */
 	public Reserva() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 724, 484);
+		setBounds(100, 100, 543, 517);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton buscarBNT = new JButton("Buscar Libro");
 		buscarBNT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -58,50 +65,110 @@ public class Reserva extends JFrame {
 		});
 		buscarBNT.setBounds(134, 221, 124, 21);
 		contentPane.add(buscarBNT);
-		
+
 		libroTXT = new JTextField();
 		libroTXT.setBounds(149, 54, 96, 19);
 		contentPane.add(libroTXT);
 		libroTXT.setColumns(10);
-		
+
 		nombreTXT = new JTextField();
 		nombreTXT.setColumns(10);
 		nombreTXT.setBounds(149, 93, 96, 19);
 		contentPane.add(nombreTXT);
-		
+
 		stockTXT = new JTextField();
 		stockTXT.setColumns(10);
 		stockTXT.setBounds(149, 133, 96, 19);
 		contentPane.add(stockTXT);
-		
+
 		estadoTXT = new JTextField();
 		estadoTXT.setColumns(10);
 		estadoTXT.setBounds(149, 177, 96, 19);
 		contentPane.add(estadoTXT);
-		
+
 		JLabel lblNewLabel = new JLabel("Codigo Libro");
 		lblNewLabel.setBounds(56, 57, 88, 13);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblNombre = new JLabel("Nombre");
 		lblNombre.setBounds(56, 96, 68, 13);
 		contentPane.add(lblNombre);
-		
+
 		JLabel lblStock = new JLabel("Stock");
 		lblStock.setBounds(56, 136, 68, 13);
 		contentPane.add(lblStock);
-		
+
 		JLabel lblEstado = new JLabel("Estado");
 		lblEstado.setBounds(56, 180, 68, 13);
 		contentPane.add(lblEstado);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(24, 252, 452, 144);
+		contentPane.add(scrollPane);
+
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"", "", "", ""},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"Categoria", "Libro", "Estado", "Stock"
+			}
+		));
+		scrollPane.setRowHeaderView(table);
+
+		JLabel lblCodigoCategoria = new JLabel("Codigo Categoria");
+		lblCodigoCategoria.setBounds(24, 419, 88, 13);
+		contentPane.add(lblCodigoCategoria);
+
+		categoriaIDTXT = new JTextField();
+		categoriaIDTXT.setColumns(10);
+		categoriaIDTXT.setBounds(117, 416, 96, 19);
+		contentPane.add(categoriaIDTXT);
+
+		categoriaBNT = new JButton("Buscar Categoria");
+		categoriaBNT.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buscarCategoria();
+			}
+		});
+		categoriaBNT.setBounds(223, 415, 124, 21);
+		contentPane.add(categoriaBNT);
 	}
-	
+
 	void buscar() {
 		Libro libro = new Libro();
 		LibroCLI lcli = new LibroCLI();
-		libro=lcli.getLibro( Integer.parseInt(libroTXT.getText()));
+		libro = lcli.getLibro(Integer.parseInt(libroTXT.getText()));
 		nombreTXT.setText(libro.getNombreLibro());
 		stockTXT.setText(String.valueOf(libro.getStock()));
 		estadoTXT.setText(libro.getEstado());
+	}
+
+	void buscarCategoria() {
+		LibroCLI lcli = new LibroCLI();
+		System.out.println(lcli.listarLibro(Integer.parseInt(categoriaIDTXT.getText())));
+		//List<Libro> listarLibro =lcli.listarLibro(Integer.parseInt(categoriaIDTXT.getText()));
+		
+		
+		DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+        for (int i = 0; i < table.getRowCount(); i++) {
+            modelo.removeRow(i);
+            i -= 1;
+        }
+        for (Libro l : lcli.listarLibro(Integer.parseInt(categoriaIDTXT.getText()))) {
+            Object[] rowData = {l.getCategoria().getCategoria(), l.getNombreLibro(), l.getEstado(), l.getStock()};
+            modelo.addRow(rowData);
+        }
+        table.setModel(modelo);
+		
 	}
 }
